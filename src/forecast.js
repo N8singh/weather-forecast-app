adb15f17a4748cfc64b6f4700f299a8e156b487b
Skuad <skuad@Skuads-MacBook-Pro.local>
2023-02-05 00:31:12 +0530

    humidity added

--- src/forecast.js
+++ src/forecast.js
@@ -4,12 +4,12 @@ const forecast = (lat,lon,callback) => {
    const url = `http://api.weatherstack.com/current?access_key=fbfdc0e7456134cf97e3cd2acd0c6ccb&query=${lat},${lon}&units=m`
 
     request({url, json : true}, (error,{body}) => {
-        const {weather_descriptions: mausam, temperature,feelslike} = body.current
+        const {weather_descriptions: mausam, temperature,feelslike,humidity} = body.current
     if(error){
         callback('Unable to connect with weatherstack api',undefined)
     }
     else{
-    callback(undefined,`The weather is ${mausam} and It's currently ${temperature} degrees out. But it feels like ${feelslike} degrees out! `)
+    callback(undefined,`The weather is ${mausam} and It's currently ${temperature} degrees out. But it feels like ${feelslike} degrees out! Also, the humidity is ${humidity} %`)
     }
 } )
 
